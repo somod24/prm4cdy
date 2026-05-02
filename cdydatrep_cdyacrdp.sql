bcfb13c08f1b702d6de667f5c8f8f79157cf562d
somod24 <oknarf55@icloud.com>
2026-05-02 14:05:16 +0200

    Create cdydatrep_cdyacrdp.sql

---
+++ cdydatrep_cdyacrdp.sql
@@ -0,0 +1,88 @@
+
+DROP TABLE if exists trans.cdyacrdp;
+
+CREATE TABLE trans.cdyacrdp (
+	item_ix int4 NULL,
+	pname varchar(6) NULL,
+	dvi float8 NULL,
+	pval float8 NULL
+);
+
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'bbch',1.0,75.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'bbch',2.0,100.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'kex',0.0,0.46);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'kex',1.0,0.56);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'kex',2.0,0.62);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prt',0.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prt',1.8,0.5);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prt',2.0,0.55);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prx',0.6,0.6);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'plx',0.0,0.5);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'bbch',0.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prx',0.0,0.3);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'plx',0.0,0.5);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'plx',0.5,0.5);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'plx',0.6,0.4);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'plx',2.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prx',0.0,0.3);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prx',0.4,0.3);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prx',0.5,0.25);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prx',0.6,0.19);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prx',0.7,0.13);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prx',0.8,0.1);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prx',2.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'bbch',0.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'bbch',1.0,50.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'bbch',2.0,100.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'kex',0.0,0.46);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'kex',1.0,0.56);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'kex',2.0,0.62);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prt',0.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prt',1.8,0.5);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prt',2.0,0.55);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prt',1.0,1.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prt',0.0,1.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prx',2.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'bbch',0.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'bbch',2.0,100.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'kex',0.0,0.46);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'kex',1.0,0.56);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'kex',2.0,0.62);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prx',0.4,0.3);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'plx',0.5,0.2);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prx',0.5,0.8);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'plx',0.6,0.1);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prx',0.7,0.4);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prx',0.8,0.4);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prx',1.0,0.2);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'plx',2.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prx',2.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'bbch',1.0,75.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'plx',0.755,0.15);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'plx',2.0,0.05);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prt',1.8,1.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prt',2.0,1.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prx',0.4,0.3);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prx',0.5,0.2);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prx',0.6,0.13);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prx',0.7,0.1);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prx',0.8,0.1);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'plx',0.25,0.2);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prx',0.0,0.3);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'plx',0.0,0.2);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'plx',1.0,0.1);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'ndfa',0.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'ndfa',2.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'ndfa',0.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'ndfa',2.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'ndfa',0.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'ndfa',2.0,0.0);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'prt',1.0,0.0001);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'plx',1.0,0.0001);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prx',1.0,0.0001);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'prt',1.0,0.0001);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'plx',1.0,0.0001);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'prx',1.0,0.0001);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (705,'ndfa',1.0,0.0001);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (700,'ndfa',1.0,0.0001);
+INSERT INTO trans.cdyacrdp (item_ix,pname,dvi,pval) VALUES (703,'ndfa',1.0,0.0001);
